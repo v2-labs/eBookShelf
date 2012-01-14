1997a6c7399a0687501bce5358555ca68c86eec8
Juvenal A. Silva Jr <juvenal.silva.jr@gmail.com>
2012-01-14 03:07:00 -0200

    First commit of the project.

---
+++ eBookShelf/MBSDocument.h
@@ -0,0 +1,13 @@
+//
+//  MBSDocument.h
+//  eBookShelf
+//
+//  Created by Juvenal Silva on 05/12/2011.
+//  Copyright (c) 2011 Software Architect. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface MBSDocument : NSDocument
+
+@end
---
+++ eBookShelf/main.m
@@ -0,0 +1,13 @@
+//
+//  main.m
+//  eBookShelf
+//
+//  Created by Juvenal Silva on 05/12/2011.
+//  Copyright (c) 2011 Software Architect. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, char *argv[]) {
+    return NSApplicationMain(argc, (const char **)argv);
+}
