75699c9f06f7f4bab73500cdb42d678b13a95dfb
Juvenal A. Silva Jr <juvenal.silva.jr@gmail.com>
2013-07-01 00:19:22 -0300

    Changes for Xcode 5
    - Adjust the files for another base source path;
    - Prepare the new settings for building on Xcode 5.

---
+++ MacOS X/main.m
@@ -0,0 +1,24 @@
+//
+//  main.m
+//  eBookShelf
+//
+//  Created by Juvenal Silva on 05/12/2011.
+//  Copyright (c) 2011 v2 Software. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+/**
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ */
+int main(int argc, char **argv) {
+    return NSApplicationMain(argc, (const char **)argv);
+}
