d7288c1acb6813cccda114c8e06ae94b536d00ac
Juvenal A. Silva Jr <juvenal.silva.jr@gmail.com>
2014-07-02 19:51:35 -0300

    Adjust the i18n base IB files
    This patch changes the i18n e l11n settings of the project to be compatible to previous OS X versions (being the maximum 10.7). Newer Base setting only works with versions 10.8 and up of OS X.

---
+++ MacOS X/V2BSMainLibraryToolbarDelegate.h
@@ -0,0 +1,13 @@
+//
+//  V2BSMainLibraryToolbarDelegate.h
+//  eBookShelf
+//
+//  Created by Juvenal A. Silva Jr. on 6/30/14.
+//  Copyright (c) 2014 v2 lab. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface V2BSMainLibraryToolbarDelegate : NSObject
+
+@end
---
+++ MacOS X/V2BSMainLibraryToolbarDelegate.m
@@ -0,0 +1,13 @@
+//
+//  V2BSMainLibraryToolbarDelegate.m
+//  eBookShelf
+//
+//  Created by Juvenal A. Silva Jr. on 6/30/14.
+//  Copyright (c) 2014 v2 lab. All rights reserved.
+//
+
+#import "V2BSMainLibraryToolbarDelegate.h"
+
+@implementation V2BSMainLibraryToolbarDelegate
+
+@end
