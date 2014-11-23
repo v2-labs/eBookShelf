37a121a662ac6bf8f0e6b900c3cdacc1d845879a
Juvenal A. Silva Jr <juvenal.silva.jr@gmail.com>
2014-11-23 00:03:22 -0200

    Set a standard on initial comments
    This patch changes all the initial file comments to follow the same standard. They were all different in content and format. Now there is a standard to follow when creating them, and all of them looks the same now, with the same required content.

--- OSX/DocumentViewer/V2BSComicViewManager.m
+++ OSX/DocumentViewer/V2BSComicViewManager.m
@@ -1,9 +1,25 @@
 //
-//  eBookShelf
-//  V2BSComicViewManager.m
+// eBookShelf
+// V2BSComicViewManager.m
 //
-//  Created by Juvenal A. Silva Jr. on 7/4/14.
-//  Copyright (c) 2014 v2 lab. All rights reserved.
+// Created by Juvenal A. Silva Jr. <juvenal.silva@mac.com>
+// Copyright (c) 2014 v2 labs. All rights reserved
+//
+// This program is free software; you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation; either version 2 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program; if not, write to the Free Software
+// Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+// MA 02110-1301, USA.
+//
 //
 
 #import "V2BSComicViewManager.h"
